c340f6caa9afe0e9f89ee1954be2945b9d8be519
foreman23 <foremanb@oregonstate.edu>
2023-12-17 14:51:34 -0800

    Boiler for basic navbar

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,28 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Nav} from 'react-bootstrap';
 import { Header } from 'semantic-ui-react';
-
+import Navbar from './components/Navbar';
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>
-        <Container className='MainContainer'>
-            <Header>Welcome to the Home Page</Header>
-        </Container>
-    </div>
-  )
+    return (
+        <div>
+
+            <Navbar></Navbar>
+
+            <Container className='MainContainer'>
+                <Row>
+                    <Col>
+                        <h1>Brandon Foreman</h1>
+                        <h3>Software Engineer based in Portland, OR</h3>
+                    </Col>
+                    <Col>
+                        2 of 2
+                    </Col>
+                </Row>
+
+            </Container>
+        </div>
+    )
 }
 
 export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const NavigationBar = () => {
+    return (
+        <div>
+            <Navbar style={{ marginBottom: '5%' }} expand="lg" className="NavBar">
+                <Container style={{ display: 'flex' }}>
+                    <Navbar.Brand style={{ marginLeft: '10%', fontWeight: 'bold' }} href="/">Brandon Foreman</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse style={{ marginLeft: '45%' }} id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="#link">About</Nav.Link>
+                            <Nav.Link href="#link">Projects</Nav.Link>
+                            <Nav.Link href="#link">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavigationBar
