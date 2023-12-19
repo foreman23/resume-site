bb47faeb10f1ca68ba20661503856453c839e35d
foreman23 <foremanb@oregonstate.edu>
2023-12-18 16:11:53 -0800

    added description text to home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col} from 'react-bootstrap';
-import { Image, Icon, Button } from 'semantic-ui-react';
+import { Image, Icon, Button, Divider } from 'semantic-ui-react';
 import Navbar from './components/Navbar';
 import React from 'react'
 
@@ -13,8 +13,9 @@ const Home = () => {
                 <Row>
                     <Col lg={7}>
                         <div className='MainLeftColumn'>
-                            <h1>Brandon Foreman</h1>
+                            <h1 style={{ color: '#0E8A5F', fontWeight: 'bolder', fontSize: 32 }}>Brandon Foreman</h1>
                             <h3>Software Engineer based in Portland, OR.</h3>
+                            <p style={{ width: '80%', fontSize: 16 }}>Hi, I’m Brandon, a Software Engineer located in Portland, Oregon. I have experience building all sorts of applications using JavaScript, React.js, Python, and more. My passion is in web development, both frontend and backend, but I enjoy and am knowledgeable in all aspects of software development. I have worked on projects for clients as well as for my own personal initiatives.</p>
                             <Row>
                                 <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: 10, marginBottom: 20 }}>
                                     <p style={{ margin: 0, marginRight: 10 }}>Follow Me</p>
@@ -24,7 +25,7 @@ const Home = () => {
                             </Row>
                             <Row>
                                 <div>
-                                    <Button href='mailto:bforemana@gmail.com' primary>Send a Message</Button>
+                                    <Button href='mailto:bforemana@gmail.com' style={{backgroundColor: '#0E8A5F', color: 'white'}}>Send a Message</Button>
                                     <Button href='https://drive.google.com/file/d/1VbkeZc9g1s4rV7L2N8K2rsKbaQoJDLL6/view?usp=sharing' target='_blank'>My Resume</Button>
                                 </div>
                             </Row>
@@ -32,10 +33,39 @@ const Home = () => {
                     </Col>
                     <Col lg={5}>
                         <div className='MainRightColumn'>
-                            <Image circular src='/placeholder_circle.png'></Image>
+                            <Image size='large' circular src='/placeholder_circle.png'></Image>
                         </div>
                     </Col>
                 </Row>
+
+                {/* <Row style={{ justifyContent:'center' }}>
+                    <div style={{ marginTop: 105, marginBottom: 70, width: '85%' }}>
+                        <h2 style={{ justifyContent: 'center', textAlign: 'center' }}>Projects</h2>
+                        <Divider></Divider>
+                        <Row>
+                            <Col>
+                                <Image src='https://placehold.co/600x400/EEE/31343C'></Image>
+                            </Col>
+                            <Col>
+                                <Image src='https://placehold.co/600x400/EEE/31343C'></Image>
+                            </Col>
+                            <Col>
+                                <Image src='https://placehold.co/600x400/EEE/31343C'></Image>
+                            </Col>
+                        </Row>
+                        <Row style={{ marginTop: 20 }}>
+                            <Col>
+                                <Image src='https://placehold.co/600x400/EEE/31343C'></Image>
+                            </Col>
+                            <Col>
+                                <Image src='https://placehold.co/600x400/EEE/31343C'></Image>
+                            </Col>
+                            <Col>
+                                <Image src='https://placehold.co/600x400/EEE/31343C'></Image>
+                            </Col>
+                        </Row>
+                    </div>
+                </Row> */}
 
             </Container>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,13 @@ const NavigationBar = () => {
         <div>
             <Navbar style={{ marginBottom: '5%' }} expand="lg" className="NavBar">
                 <Container style={{ display: 'flex' }}>
-                    <Navbar.Brand style={{ marginLeft: '10%', fontWeight: 'bold' }} href="/">Brandon Foreman</Navbar.Brand>
+                    <Navbar.Brand style={{ fontWeight: 'bold' }} href="/">Brandon Foreman</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse style={{ marginLeft: '45%' }} id="basic-navbar-nav">
+                    <Navbar.Collapse style={{ marginLeft: '2%' }} id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="#link">About</Nav.Link>
                             <Nav.Link href="#link">Projects</Nav.Link>
+                            <Nav.Link href="#link">Resume</Nav.Link>
                             <Nav.Link href="#link">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
